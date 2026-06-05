c31c1384d5d3cffde05f5d1557a926ce6554383e
Philasande Mathafeni <labale1996@gmail.com>
2026-06-05 17:12:34 +0200

    Completed variables and datatypes revision. Added comments to explain that Python automatically assigns datatypes based on the value assigned to the variable, and that there is no need to declare the datatype explicitly.

--- variables_and_datatypes.py
+++ variables_and_datatypes.py
@@ -1,3 +1,22 @@
 #Revising Python variables and datatypes
+#No need to declare the datatype, Python  automatically assigns the datatype based on the value assigned to the variable
 
+
+#int
+age = 90
+
+#Float 
+pi = 3.14
+
+#String
+Fullname = "Philasande Mathafeni"
+
+#Boolean
+is_adult = True
+
+#Printing the variables and their datatypes
+print("Age:", age, "Datatype:", type(age))
+print("Pi:", pi, "Datatype:", type(pi))
+print("Fullname:", Fullname, "Datatype:", type(Fullname))
+print("Is Adult:", is_adult, "Datatype:", type(is_adult))   
 
