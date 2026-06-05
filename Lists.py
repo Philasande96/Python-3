0e121b3b8a33c6ea7dfd0958e5bc868fd87c1f69
Philasande Mathafeni <labale1996@gmail.com>
2026-06-05 17:59:19 +0200

    Done with Lists

---
+++ Lists.py
@@ -0,0 +1,12 @@
+#Introduction to lists in Python
+#Lists are ordered collections of items that can be of different types. They are mutable, meaning you can change their contents after they have been created.
+
+#Create and initialize a list
+provinces =['Eastern Cape', 'Free State', 'Gauteng', 'KwaZulu-Natal', 'Limpopo', 'Mpumalanga', 'North West', 'Northern Cape', 'Western Cape']
+
+#print the list
+print(provinces) #This method will print the entire list as it is, includind characters and brackets.
+
+#To print each item in the list separately, we can use a for loop:
+for i in provinces:
+    print(i)
