37c7ab740dc91ab033efa5e39a7a712dce654cf8
Philasande Mathafeni <labale1996@gmail.com>
2026-06-05 18:17:59 +0200

    working with lists

--- Lists.py
+++ Lists.py
@@ -6,17 +6,19 @@ provinces =['Eastern Cape', 'Free State', 'Gauteng', 'KwaZulu-Natal', 'Limpopo',
 
 #print the list
 print(provinces) #This method will print the entire list as it is, includind characters and brackets.
-    """
+"""
     Print Output:
 ['Eastern Cape', 'Free State', 'Gauteng', 'KwaZulu-Natal', 'Limpopo', 'Mpumalanga', 'North West', 'Northern Cape', 'Western Cape']
-    """
+"""
+print(" ") #This method will print the first item in the list, which is 'Eastern Cape'.")
+
 
 #To print each item in the list separately, we can use a for loop:
 for i in provinces:
-    print(i)
+    print(i) #The title() method capitalizes the first letter of each word in the string.
     
-    """
-    Eastern Cape
+"""
+Eastern Cape
 Free State
 Gauteng
 KwaZulu-Natal
@@ -25,4 +27,21 @@ Mpumalanga
 North West
 Northern Cape
 Western Cape
-    """
+"""
+    
+    #Modifying the list
+    
+towns =['Port Elizabeth', 'Bloemfontein', 'Johannesburg', 'Durban', 'Polokwane', 'Nelspruit', 'Mahikeng', 'Kimberley', 'Cape Town']
+
+print(towns) #This method will print the entire list as it is, includind characters and brackets.
+#Changing the town in position(index 0)
+towns[0] = 'East London' #This method will change the first item in the list, which is 'Port Elizabeth', to 'East London'.
+print(towns) #This method will print the modified list with 'East London' instead of 'Port Elizabeth'.
+"""
+Original
+['Port Elizabeth', 'Bloemfontein', 'Johannesburg', 'Durban', 'Polokwane', 'Nelspruit', 'Mahikeng', 'Kimberley', 'Cape Town']
+
+Newly implemented
+['East London', 'Bloemfontein', 'Johannesburg', 'Durban', 'Polokwane', 'Nelspruit', 'Mahikeng', 'Kimberley', 'Cape Town']
+"""
+
