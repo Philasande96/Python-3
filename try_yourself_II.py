f2ea051669b2bb37a186100a8b7e2c9e663f6c7a
Philasande Mathafeni <labale1996@gmail.com>
2026-06-07 12:34:22 +0200

    Done

---
+++ try_yourself_II.py
@@ -0,0 +1,7 @@
+
+#3-4 Guest list: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you'd like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+invitees = ['Albert Einstein', 'Marie Curie', 'Leonardo da Vinci','Philasande']#declare a list of invitees
+for invitee in invitees:#loop through the list of invitees
+    print(f"Dear {invitee}, I would like to invite you to dinner. It would be an honor to have you as my guest.")
+
