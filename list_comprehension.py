d932b873661ed98cc793000c55c22c83b7ed7e70
Philasande Mathafeni <labale1996@gmail.com>
2026-06-12 20:51:50 +0200

    /////

---
+++ list_comprehension.py
@@ -0,0 +1,8 @@
+#List comprehension is the process of creating a new list based on an existing list. It is a concise way to create lists in Python. The syntax for list comprehension is as follows:
+
+newlist = [values**2 for values in range(1,11)]#create a new list of squares of numbers from 1 to 10
+print(newlist)
+
+newMe = newlist[:3]
+
+print(newMe)
