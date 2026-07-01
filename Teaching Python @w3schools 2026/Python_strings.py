42a236a159c02b3b9b23f83f52f16dd9fb185fac
Mathafeni Philasande <labale1996@gmail.com>
2026-07-01 13:06:31 +0200

    Initial Commit

---
+++ Teaching Python @w3schools 2026/Python_strings.py
@@ -0,0 +1,30 @@
+#learn more about Python strings
+
+"""
+String in Python are referred to the sequence of characters, which can be of any length. Strings are enclosed in either single quotes (') or double quotes (").
+Strings can be concatenated using the + operator.
+Strings can be indexed and sliced.
+"""
+#Example 1
+my_name = "Philasande"
+print(my_name) #output Philasande
+
+#Example 2: Combining two strings 
+name = 'Philasande'
+surname= 'Mathafeni'
+
+print (name + " " + surname) #Output Philasande Mathafeni
+
+#NB: Strings in Python can be sorrounded by single or double quotation marks
+#example 3
+single_quote = 'I am a single quote'
+double_quote = "I am a double quote"
+
+#Strings  variables contain literals. In Short, string value  in Python is referred to literal 
+
+#NB: You can used quotes inside string  as long they don't match the quotes surrounding the entire literal 
+
+#example 4
+
+my_self = "I'm Philasande Mathafeni, Liqhame's father"
+print(my_self) #Output I'm Philasane Mathafeni, Liqhame's father
