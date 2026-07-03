eb27d287f7b01fb67db6d2cb317450d9df3c072b
Mathafeni Philasande <labale1996@gmail.com>
2026-07-03 19:23:04 +0200

    Done about operators

--- Teaching Python @w3schools 2026/Python_operators.py
+++ Teaching Python @w3schools 2026/Python_operators.py
@@ -23,7 +23,7 @@ y = 10
 print(x + y) #Output 100
 
 k = '10'
-l = '20'
+l = '20'  # noqa: E741
 print(k + l) #Output 1020 --> Concatenation of two strings , if you needto perform numeric calculation, you need to convert the string to integer using int() function
 print(int(k) + int(l)) #Output 30 --> Addition of two numbers after converting the string to integer using int() function 
 
@@ -51,7 +51,7 @@ y = 10
 print(x * y) #Output 900
 
 k = '10'
-l = '20'
+l = '20'  # noqa: E741
 print(int(k) * int(l)) #Output 200 --> Multiplication of two numbers after converting the string to integer using int() function
 
 #Division operator
@@ -62,7 +62,7 @@ y = 10
 print(x / y) #Output 9
 
 k = '10'
-l = '20'
+l = '20'  # noqa: E741
 print(int(k) / int(l)) #Output 5 --> Division of two numbers after converting the string to integer using int() function
 
 #Modulus operator
@@ -71,7 +71,7 @@ y = 10
 print(x % y) #Output 0
 
 k = '10'
-l = '20'
+l = '20'  # noqa: E741
 print(int(k) % int(l)) #Output 0 --> Modulus of two numbers after converting the string to integer using int() function
 
 #Exponentiation operator
@@ -80,7 +80,7 @@ y = 10
 print(x ** 2) #Output 8100
 print(y ** 2) #Output 100
 k = '10'
-l = '2'
+l = '2'  # noqa: E741
 print(int(k) ** int(l)) #Output 100 --> Exponentiation of two numbers after converting the string to integer using int() function
 
 
@@ -90,7 +90,7 @@ y = 10
 print(x // y) #Output 9
 
 k = '10'
-l = '2'
+l = '2'  # noqa: E741
 print(int(k) // int(l)) #Output 5 --> Floor Division of two numbers after converting the string to integer using int() function
 
 #Next will be the Assignment Operators
@@ -128,6 +128,11 @@ print(f"The list has {len(count)} elements.")
 # Using Walrus operator 
 if(n:= len(count)) > 2:
     print(f"\nThe list has {n} elements.\nWalrus operator")
+    
+name = 'Philasande'
+if(n:= len(name)) > 2:
+    print(f"\nThe name has {n} characters.\nWalrus operator")
+    
 #I will skip others , I will create a page for them separately
 
 #Ternary Operator
@@ -147,3 +152,127 @@ pass_mark = 60
 mark = 45
 results = 'Pass' if mark >= pass_mark else 'Fail'
 print(f"\n{mark}% is {results}, try next year.\nTernary operator used in example 3")
+
+
+#Comparison Operators 
+"""
+Comparison operators are used to compare two or more values in a code or instruction. 
+| Operator | Description |
+| --- | --- |
+| == | Equal to | e.g  x ==y
+| != | Not equal to | e.g x != y
+| > | Greater than | e.g x > y
+| < | Less than | e.g x < y
+| >= | Greater than or equal to | e.g x >= y
+| <= | Less than or equal to |  e.g x <= y
+"""
+
+#Equal operator
+x = 10
+y = 10
+print(x == y) #Output True
+
+#Not equal operator
+x = 10
+y = 20
+print(x != y) #Output True
+
+#Greater than operator
+x = 10
+y = 20
+print(x > y) #Output False
+
+#Less than operator
+x = 10
+y = 20
+print(x < y) #Output True
+
+#Greater than or equal to operator
+x = 10
+y = 20
+print(x >= y) #Output False
+
+#Less than or equal to operator
+x = 10
+y = 20
+print(x <= y) #Output True
+
+#Logical Operators
+"""
+Logical operators are used to combine multiple conditions in a single expression. 
+| Operator | Description |
+| --- | --- |
+| and | Returns True if both conditions are True |
+| or | Returns True if at least one of the conditions is True |
+| not | Returns the opposite of the condition |
+"""
+
+#and operator
+x = 10
+y = 20
+print(x > 5 and y < 10) #Output False
+
+#or operator
+x = 10
+y = 20
+print(x > 5 or y < 10) #Output True
+
+#not operator
+x = 10
+y = 20
+print(not x > 5) #Output False
+print(not y < 10) #Output True
+
+#Identity Operators
+"""
+Identity operators are used to compare two or more values in a code or instruction. 
+| Operator | Description |
+| --- | --- |
+| is | Returns True if both values are the same |
+| is not | Returns True if both values are not the same |
+"""
+
+#is operator
+x = 10
+y = 10
+print(x is y) #Output True
+
+#is not operator
+x = 10
+y = 20
+print(x is not y) #Output True
+print(y is not y) #Output False
+
+#Membership Operators
+"""
+Membership operators are used to check if a value is present in a sequence or collection. 
+| Operator | Description |
+| --- | --- |
+| in | Returns True if the value is present in the sequence or collection | 
+| not in | Returns True if the value is not present in the sequence or collection |
+"""
+numbers = [1, 2, 3, 4, 5]
+print(2 in numbers) #Output True
+print(6 in numbers) #Output False
+
+#not in operator
+numbers = [1, 2, 3, 4, 5]
+print(2 not in numbers) #Output False
+print(6 not in numbers) #Output True
+
+#Bitwise Operators
+"""
+Bitwise operators are used to perform operations on binary data. 
+| Operator | Description |
+| --- | --- |
+| & | Bitwise AND | Returns the bitwise AND of two values |
+| \| | Bitwise OR | Returns the bitwise OR of two values |      
+| ^ | Bitwise XOR | Returns the bitwise XOR of two values |
+| ~ | Bitwise NOT | Returns the bitwise NOT of a value |
+| << | Left shift | Returns the left shift of a value |
+| >> | Right shift | Returns the right shift of a value |
+"""
+
+#I will deal with them in the next module where we will be writing instructions to reveal the binary number of any decimal number system
+
+
